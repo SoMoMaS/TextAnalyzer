c4dda895788376f7f7a030bd7f647d4b67329240
Soma Molnar <soma.matyas.molnar@gmail.com>
2024-06-14 10:37:17 +0200

    Added base backend project

---
+++ backend/text-analyzer/src/test/java/com/dedalus/text_analyzer/TextAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dedalus.text_analyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TextAnalyzerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
